4137df116085175f0a56386dca194e6af7e3051e
Unknown <gmlwns7430@naver.com>
2019-10-06 14:42:05 +0900

    Add router

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const userRoute = require("./routes/user");
+
+dotenv.config();
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
 
+
+mongoose.connect(process.env.MONGO_URI, 
+    {
+        useNewUrlParser:true, 
+        useCreateIndex:true, 
+        useUnifiedTopology:true
+    })
+    .then(() => console.log("mongodb connected .. "))
+    .catch(err => console.log(err));
+
+
+app.use("/user", userRoute);
 const PORT = process.env.PORT || 7000;
 
 
